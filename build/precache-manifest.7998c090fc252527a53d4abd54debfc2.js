c84e777e545b76b57be7323a5eb9a59d9017ed1c
Babak <babakjahangiri@gmail.com>
2020-07-16 23:25:14 +0100

    npm run deploy done

---
+++ build/precache-manifest.7998c090fc252527a53d4abd54debfc2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ac75752d6fbd0e8aa1762f3849b3e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae38020673954a5a3a3",
+    "url": "/static/css/main.692d6f24.chunk.css"
+  },
+  {
+    "revision": "a53b18101a0d541fea35",
+    "url": "/static/js/2.e2809d28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2809d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae38020673954a5a3a3",
+    "url": "/static/js/main.dc183cc3.chunk.js"
+  },
+  {
+    "revision": "fadae01d5cc4f8c3881f",
+    "url": "/static/js/runtime-main.b3965851.js"
+  },
+  {
+    "revision": "48aa2bc8d05a0a4883689647d0584934",
+    "url": "/static/media/cart-ico.48aa2bc8.svg"
+  },
+  {
+    "revision": "304f6e9afa15580b84a9282330a50606",
+    "url": "/static/media/header_hero.304f6e9a.jpg"
+  },
+  {
+    "revision": "0fe42dc736345b70d749f294ba24e43a",
+    "url": "/static/media/mk_watch.0fe42dc7.jpg"
+  },
+  {
+    "revision": "37f2ff9b9445bfce53ac738731d39d02",
+    "url": "/static/media/remove.37f2ff9b.svg"
+  },
+  {
+    "revision": "1641a7e4aa55418c680e02fe40fa024a",
+    "url": "/static/media/wslogo.1641a7e4.svg"
+  }
+]);
